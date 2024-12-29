de66389b7127997125df98f9bb0641653773dda9
mohamedDVP <mohamed.mak@laposte.net>
2024-12-29 19:43:12 +0100

    crud admin complet dans sidebar

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Avis;
+use App\Entity\Configuration;
+use App\Entity\Covoiturage;
+use App\Entity\Marque;
+use App\Entity\Parametre;
+use App\Entity\Role;
 use App\Entity\User;
+use App\Entity\Voiture;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -38,6 +45,19 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section("Gestion des utilisateurs");
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Roles','fas fa-user', Role::class);
+        yield MenuItem::section("Gestion des covoiturages");
+        yield MenuItem::linkToCrud('Covoiturages','fas fa-flag-checkered', Covoiturage::class);
+        yield MenuItem::linkToCrud('Voiture','fas fa-car', Voiture::class);
+        yield MenuItem::linkToCrud('Marque','fas fa-trademark', Marque::class);
+        yield MenuItem::section("Gestion des avis");
+        yield MenuItem::linkToCrud('Avis','fas fa-stars', Avis::class);
+        yield MenuItem::section('Parametres');
+        yield MenuItem::linkToCrud('Configuration','fas fa-sliders', Configuration::class);
+        yield MenuItem::linkToCrud('Paramètres','fas fa-gear', Parametre::class);
+
+
     }
 }
