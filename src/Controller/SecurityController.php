290ca00a4eabfe67a7bed41dbab49e943aad9d2b
mohamedDVP <mohamed.mak@laposte.net>
2025-04-29 19:25:44 +0200

    affichage du nom de l'utilisateur dans la navbar

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -25,6 +26,6 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         // Controller pour la déconnexion, mais Symfony gère directement la déconnexion.
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
