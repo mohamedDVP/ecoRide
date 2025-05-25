559df2f45b49ec662d7120f09d989b16bd7fa42c
mohamedDVP <mohamed.mak@laposte.net>
2025-05-25 17:33:59 +0200

    add security and usercrud controllers to branch develop

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,6 +26,6 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         // Controller pour la déconnexion, mais Symfony gère directement la déconnexion.
-        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
